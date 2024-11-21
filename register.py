81c2ecd4a4321e4c70e4219ff430f111da9a61b7
andres2631996 <andres2631996@hotmail.com>
2024-11-21 11:20:17 +0100

    Modify code to register stuff back to atlas

--- register.py
+++ register.py
@@ -9,27 +9,27 @@ import shutil
 
 if __name__ == "__main__":
     ctp_folder = "/scratch/amartinezmora/raw_data/ctp/mrclean_late_30002"
-    atlas_file = "/scratch/amartinezmora/raw_data/cta/mrclean_late_30002.nii.gz"
-    out_folder = "/scratch/amartinezmora/raw_data/cta_registered_to_ctp"
+    atlas_file = "/scratch/amartinezmora/raw_data/atlas/atlas.nii.gz"
+    out_folder = "/scratch/amartinezmora/raw_data/ctp_registered_to_atlas"
 
     assert os.path.exists(ctp_folder) and os.path.exists(atlas_file), f"CTP folder '{ctp_folder}' or atlas file '{atlas_file}' do not exist"
 
     if not(os.path.exists(out_folder)):
         os.makedirs(out_folder)
 
-    #cid = os.path.basename(ctp_folder)
-    #out_folder = os.path.join(out_folder, cid)
-    #if os.path.exists(out_folder):
-    #    shutil.rmtree(out_folder)
+    cid = os.path.basename(ctp_folder)
+    out_folder = os.path.join(out_folder, cid)
+    if os.path.exists(out_folder):
+        shutil.rmtree(out_folder)
 
     # Copy folder into destination, to do all the registration stuff in there
-    #shutil.copytree(ctp_folder, out_folder)
+    shutil.copytree(ctp_folder, out_folder)
     #outfile = os.path.join(out_folder, os.path.basename(atlas_file))
     #shutil.copy(atlas_file, out_folder)
 
     # Load first frame of CTP and atlas
-    ctp_frame_files = sorted(os.listdir(ctp_folder))
-    ctp_frame_files = [os.path.join(ctp_folder, f) for f in ctp_frame_files if ".nii.gz" in f]
+    ctp_frame_files = sorted(os.listdir(out_folder))
+    ctp_frame_files = [os.path.join(out_folder, f) for f in ctp_frame_files if ".nii.gz" in f]
     t0_file = ctp_frame_files[0]
     t0_image = sitk.ReadImage(t0_file)
     atlas_image = sitk.ReadImage(atlas_file)
@@ -37,17 +37,17 @@ if __name__ == "__main__":
     # Set parameters
     p = set_parameters(method='euler', DefaultPixelValue=-1024, metric='AdvancedNormalizedCorrelation')
     # Derive transformation matrix
-    transform_matrix = get_transformation_matrix(fixed=t0_image, moving=atlas_image, clipvalue=[None, None], parameters=p)
+    transform_matrix = get_transformation_matrix(fixed=atlas_image, moving=t0_image, clipvalue=[None, None], parameters=p)
 
-    atlas_image = apply_transformation(transform_matrix, moving=atlas_image, segmentation=False, default_pixel=-1024)
-    outfile = os.path.join(out_folder, os.path.basename(atlas_file))
-    sitk.WriteImage(atlas_image, outfile)
+    #atlas_image = apply_transformation(transform_matrix, moving=atlas_image, segmentation=False, default_pixel=-1024)
+    #outfile = os.path.join(out_folder, os.path.basename(atlas_file))
+    #sitk.WriteImage(atlas_image, outfile)
 
     # For each CTP frame, register it to the atlas
-    """
+    
     for frame in ctp_frame_files:
         frame_image = sitk.ReadImage(frame)
         frame_image = apply_transformation(transform_matrix, moving=frame_image, segmentation=False, default_pixel=-1024)
         outfile = os.path.join(out_folder, frame)
         sitk.WriteImage(frame_image, outfile)
-    """
+    
