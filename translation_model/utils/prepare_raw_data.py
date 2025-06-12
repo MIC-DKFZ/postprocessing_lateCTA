650f0d9e855b7f741fd0eea6b96179af7903a322
andres2631996 <andres2631996@hotmail.com>
2025-06-12 17:07:34 +0200

    Allow train-test split creation

--- translation_model/utils/prepare_raw_data.py
+++ translation_model/utils/prepare_raw_data.py
@@ -39,6 +39,7 @@ def main(args):
     time_folder = args.t
     brain_folder = args.b
     out_folder = args.o
+    split = args.s
 
     assert os.path.exists(cta_folder), f"CTA folder '{cta_folder}' does not exist"
     assert os.path.exists(dist_folder), f"Distance folder '{dist_folder}' does not exist"
@@ -49,8 +50,11 @@ def main(args):
         assert os.path.exists(brain_folder), f"Brain folder '{brain_folder}' does not exist"
         # Create output brain folder
         brain_out_folder = os.path.join(out_folder, "raw_splitted", "brainTr")
+        brain_out_test_folder = os.path.join(out_folder, "raw_splitted", "brainTs")
         if not(os.path.exists(brain_out_folder)):
-            os.makedirs(brain_out_folder) 
+            os.makedirs(brain_out_folder)
+        if not(os.path.exists(brain_out_test_folder)):
+            os.makedirs(brain_out_test_folder)
 
     # Obtain CIDs
     cta_cids = np.array([f.replace(".nii.gz", "") for f in sorted(os.listdir(cta_folder))], dtype=str) 
@@ -62,6 +66,8 @@ def main(args):
     # Set up output folders
     train_folder = os.path.join(out_folder, "raw_splitted", "imagesTr") 
     label_folder = os.path.join(out_folder, "raw_splitted", "labelsTr") 
+    test_folder = os.path.join(out_folder, "raw_splitted", "imagesTs") 
+    label_test_folder = os.path.join(out_folder, "raw_splitted", "labelsTs") 
 
     if not(os.path.exists(train_folder)):
         os.makedirs(train_folder)
@@ -69,15 +75,43 @@ def main(args):
     if not(os.path.exists(label_folder)):
         os.makedirs(label_folder)
 
+    if not(os.path.exists(test_folder)):
+        os.makedirs(test_folder)
+
+    if not(os.path.exists(label_test_folder)):
+        os.makedirs(label_test_folder)
+
+    # By default: apply a random train-test split
+    # TODO: stratify for phase 
+    sample_size = int(split*common_cids.shape[0])
+    np.random.seed(42)
+    test_cids = np.random.choice(common_cids, 
+                                 size=sample_size, 
+                                 replace=False)
+
     # Iterate through IDs
     for i in common_cids:
-        print(i) 
         # Retrieve files
         # CTA file
         cta_file = os.path.join(cta_folder, f"{i}.nii.gz")
 
         # Brain file
         brain_file = os.path.join(brain_folder, f"{i}_brain.nii.gz") if brain_folder is not None else None
+
+        # Set up output filenames
+        out_dist_file = os.path.join(label_folder, f"{i}_dist.nii.gz")
+        out_cta_file = os.path.join(train_folder, f"{i}.nii.gz")  
+        out_time_file = os.path.join(label_folder, f"{i}_time.nii.gz")
+        if os.path.exists(brain_file):
+            out_brain_file = os.path.join(brain_out_folder, f"{i}.nii.gz")
+        if i in test_cids:
+            print(i, "test")
+            out_dist_file = os.path.join(label_test_folder, f"{i}_dist.nii.gz")
+            out_cta_file = os.path.join(test_folder, f"{i}.nii.gz")  
+            out_time_file = os.path.join(label_test_folder, f"{i}_time.nii.gz")
+            out_brain_file = os.path.join(brain_out_test_folder, f"{i}.nii.gz")
+        else:
+            print(i, "train") 
 
         # Identify inferior and superior rows where to apply trimming for distance map
         # Avoid having distance information in slices where there is no CTA information 
@@ -97,31 +131,26 @@ def main(args):
         time_file = os.path.join(time_folder, f"{i}.nii.gz")
 
         # Distance file: trim it with CTA information
-        out_dist_file = os.path.join(label_folder, f"{i}_dist.nii.gz")
         if not(os.path.exists(out_dist_file)):
             dist_file = os.path.join(dist_folder, f"{i}.nii.gz") 
             dist_image = sitk.ReadImage(dist_file)
             dist_img = sitk.GetArrayFromImage(dist_image)
             dist_img[:low_lim] = dist_img.max()
-            dist_img[high_lim:] = dist_img.min()
+            dist_img[high_lim:] = dist_img.max()
             dist_image_trim = sitk.GetImageFromArray(dist_img)
             dist_image_trim.CopyInformation(dist_image)
             sitk.WriteImage(dist_image_trim, out_dist_file)  
 
         # Copy CTA file 
-        out_cta_file = os.path.join(train_folder, f"{i}.nii.gz")  
         if not(os.path.exists(out_cta_file)): 
             shutil.copyfile(cta_file, out_cta_file)
 
         # Copy brain file, if it exists
         if (brain_file is not None):
-            if os.path.exists(brain_file):
-                out_brain_file = os.path.join(brain_out_folder, f"{i}.nii.gz")
-                if not(os.path.exists(out_brain_file)):
-                    shutil.copyfile(brain_file, out_brain_file)
+            if not(os.path.exists(out_brain_file)):
+                shutil.copyfile(brain_file, out_brain_file)
 
         # Convert time image into rank image
-        out_time_file = os.path.join(label_folder, f"{i}_time.nii.gz")
         if not(os.path.exists(out_time_file)):
             time_image = sitk.ReadImage(time_file)
             time_img = sitk.GetArrayFromImage(time_image) 
@@ -139,6 +168,7 @@ def get_args():
     parser.add_argument("--d", help="Distance map folder", required=True, type=str)
     parser.add_argument("--b", help="Brain segmentation folder", default=None, type=str)
     parser.add_argument("--o", help="Output folder", required=True, type=str)
+    parser.add_argument("--s", help="Train-test split ratio", default=0.2, type=float)
     args = parser.parse_args()
 
     return args
