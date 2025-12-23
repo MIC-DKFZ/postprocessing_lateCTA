b52529fd76697e387ac5120f0a19327a9ffa9fcb
Andres Martinez Mora <andres2631996@hotmail.com>
2025-12-23 18:46:41 +0100

    Final version of result figures

--- figure_plots.py
+++ figure_plots.py
@@ -7,13 +7,13 @@ import argparse
 import time
 import os, sys
 
-plt.rcParams["font.size"] = 14  # default text
+plt.rcParams["font.size"] = 16  # default text
 plt.rcParams["font.family"] = "serif"
 plt.rcParams["axes.titlesize"] = 16  # title
-plt.rcParams["axes.labelsize"] = 14  # axis labels
-plt.rcParams["xtick.labelsize"] = 14  # x tick labels
-plt.rcParams["ytick.labelsize"] = 12  # y tick labels
-plt.rcParams["legend.fontsize"] = 12  # legend
+plt.rcParams["axes.labelsize"] = 16  # axis labels
+plt.rcParams["xtick.labelsize"] = 16  # x tick labels
+plt.rcParams["ytick.labelsize"] = 16  # y tick labels
+plt.rcParams["legend.fontsize"] = 14  # legend
 
 
 def plot_metrics_boxplots(data, title: str = "", outfile: str = ""):
@@ -30,7 +30,7 @@ def plot_metrics_boxplots(data, title: str = "", outfile: str = ""):
     """
 
     methods = list(data.keys())
-    metrics = [m for m in data[methods[0]].keys() if m != "FP"]
+    metrics = [m for m in data[methods[0]].keys() if m != "FP per image"]
 
     n_methods = len(methods)
     n_metrics = len(metrics)
@@ -49,11 +49,13 @@ def plot_metrics_boxplots(data, title: str = "", outfile: str = ""):
             )
 
     # Figure
-    fig, ax = plt.subplots(figsize=(12, 8))
+    fig, ax = plt.subplots(
+        figsize=(10, 8)
+    )  # (14,10) for external cohort // (10,8) for development cohort
 
-    metric_spacing = 1.5
+    metric_spacing = 0.3  # 1.5 for external cohort, 0.3 for development cohort
     positions = np.arange(n_metrics) * metric_spacing
-    width = 1.0 / n_methods
+    width = 0.2 / n_methods  # 0.8 for external cohort, 0.1 for development cohort
 
     ### IMPORTANT: define xticklabels ONCE, not inside the loop
     ax.set_xticks(positions)
@@ -70,14 +72,16 @@ def plot_metrics_boxplots(data, title: str = "", outfile: str = ""):
             patch_artist=True,
             showfliers=False,
             medianprops=dict(color="orange", linewidth=2),
+            whiskerprops=dict(color="black", linewidth=2),
+            capprops=dict(color="black", linewidth=2),
             manage_ticks=False,
         )
 
         # Color boxes
         for patch in box["boxes"]:
             patch.set_facecolor(colors[i])
-        for item in box["whiskers"] + box["caps"]:
-            item.set_color(colors[i])
+        # for item in box["whiskers"] + box["caps"]:
+        # item.set_color(colors[i])
 
         # Add median + mean markers
         for j, metric in enumerate(metrics):
@@ -87,14 +91,20 @@ def plot_metrics_boxplots(data, title: str = "", outfile: str = ""):
 
             x_pos = positions[j] + (i - n_methods / 2) * width + width / 2
 
+            pos = median_val + 0.001
+            # if metric == "AUROC" or metric == "Case specificity":
+            if metric == "Box precision":
+                pos = s[-1] + 0.001
+
             ax.text(
                 x_pos,
-                median_val + 0.001,
+                pos,
                 f"{median_val:.3f}",
                 ha="center",
                 va="bottom",
-                fontsize=12,
+                fontsize=14,
                 color="black",
+                fontweight="bold",
             )
 
             ax.plot(
@@ -113,7 +123,19 @@ def plot_metrics_boxplots(data, title: str = "", outfile: str = ""):
 
     # Legend
     method_handles = [Line2D([0], [0], color=colors[i], lw=5) for i in range(n_methods)]
-    method_labels = [f"{method}\n(FP={data[method]['FP']})" for method in methods]
+    # method_labels = [
+    #    f"{method}\n(FP per image={data[method]['FP per image']:.3f})"
+    #    for method in methods
+    # ]
+
+    method_labels = [
+        (
+            f"{method}\n(FP per image={data[method]['FP per image']:.3f})\n"
+            if method == "Baseline"
+            else f"False positive\nreduction\n(FP per image={data[method]['FP per image']:.3f})\n"
+        )
+        for method in methods
+    ]
 
     mean_handle = Line2D(
         [0],
